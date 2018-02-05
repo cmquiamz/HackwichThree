1c3b4fefe96dea4d0e15ff56bda805ecb5d478ea
CM Student <cmstudent@UHWO-ACM-20.local>
2018-02-05 10:52:22 -1000

    Added about icon to the navigation bar for

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var firstLabel: UILabel!
+    
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+  //set firstLabrl to the string "About"
+        
+self.firstLabel.text = "About"
+        
     }
 
     override func didReceiveMemoryWarning() {
