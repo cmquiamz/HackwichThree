b782b167ceb0dd2fa40ca375b37b4b4262e9fa47
CM Student <cmstudent@UHWO-ACM-08.local>
2018-02-27 15:40:03 -1000

    Worked on weather page

---
+++ HackwichThree/SecondViewContrller.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewContrller.swift
+//  HackwichThree
+//
+//  Created by CM Student on 2/27/18.
+//  Copyright © 2018 CM Student. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewContrller: NSObject {
+
+}
