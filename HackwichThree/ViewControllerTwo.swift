5e0b9b5d5568c76d4ed2784c228cda20bccfca3d
CM Student <cmstudent@UHWO-ACM-20.local>
2018-03-12 11:29:54 -1000

    Completed tab #2

--- HackwichThree/ViewControllerTwo.swift
+++ HackwichThree/ViewControllerTwo.swift
@@ -14,6 +14,7 @@ class ViewControllerTwo: UIViewController {
     @IBOutlet var enterWeather: UITextField!
     @IBOutlet var convertWeather: UILabel!
     
+    
  
 
     override func viewDidLoad() {
@@ -23,11 +24,21 @@ class ViewControllerTwo: UIViewController {
         
         self.navigationTitleTwo.title = "Weather Converter"
         enterWeather.placeholder = "Enter degrees Farenheit"
-        
+       self.enterWeather.text = ""
         
         
     }
 
+    
+    @IBAction func convertButtonPressed(_ sender: Any) {
+        var userInput: Float
+        userInput = Float(enterWeather.text!)!
+        var x:String
+        x = String((userInput - 32) * 5/9)
+        self.convertWeather.text = "Today is \(x) degrees."
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -44,9 +55,5 @@ class ViewControllerTwo: UIViewController {
     }
     */
 
-    @IBAction func convertButtonPressed(_ sender: Any) {
-       
-        
-
-    }
+    
 }
